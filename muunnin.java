e086aa203837858bed0dd10f3ba64d139fc80ac5
Toni K <64441448+tonitsi@users.noreply.github.com>
2020-04-30 02:53:05 +0300

    Add files via upload
    - added muunnin.java and muunnin.class

---
+++ muunnin.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+ 
+public class muunnin {
+ 
+	public static void main(String[] args) {
+ 
+		double maara, dollari, euro, punta;
+        DecimalFormat f = new DecimalFormat("##.##");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- VALUUTANMUUNNIN ---");
+        System.out.println("(Valuuttakurssit ovat laskettu 29.4.2020.)");
+		System.out.println("Anna luku: ");
+
+		maara = sc.nextFloat();
+            System.out.println(" ");
+            System.out.println("----------------------------------");
+ 
+            dollari = maara * 0.92;
+			System.out.println(maara + " dollaria on: " + f.format(dollari) + " euroa.");
+            euro = maara * 1.09;
+			System.out.println(maara + " euroa on: " + f.format(euro) + " dollaria.");
+
+			System.out.println("----------------------------------");
+
+			punta = maara * 1.15;
+			System.out.println(maara + " puntaa on: " + f.format(punta) + " euroa.");
+			punta = maara * 1.25;
+			System.out.println(maara + " puntaa on: " + f.format(punta) + " dollaria.");
+
+            System.out.println("----------------------------------");
+
+            dollari = maara * 0.80;
+			System.out.println(maara + " dollaria on: " + f.format(dollari) + " puntaa.");
+            euro = maara * 0.87;
+			System.out.println(maara + " euroa on: " + f.format(euro) + " puntaa.");
+
+            System.out.println("----------------------------------");
+            System.out.println(" ");
+
+        ArrayList<String> randomiLause = new ArrayList<>();
+        randomiLause.add("Fun fact: On olemassa yli 100 erilaista virusta, jotka aiheuttavat normaalin flunssan.");
+        randomiLause.add("Fun fact: Bermudan kolmiossa ei tilastollisesti tapahdu normaalia enemmän onnettomuuksia.");
+        randomiLause.add("Fun fact: 40% maailman viineistä valmistetaan Italiassa ja Ranskassa.");
+        randomiLause.add("Fun fact: Bill Gates aloitti ohjelmoinnin 13-vuotiaana.");
+        
+        int x = (int) (Math.random() * randomiLause.size());
+        String y = randomiLause.get(x);
+        System.out.println(y);
+        System.out.println(" ");
+
+		}
+		
+	}
+ 
