01a9dd19803c375d5d7958e3ebb2a233f0dea2c5
Toni K <64441448+tonitsi@users.noreply.github.com>
2020-04-30 09:55:04 +0300

    Add files via upload

--- muunnin.java
+++ muunnin.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 public class muunnin {
  
 	public static void main(String[] args) {
- 
+
+        // VARIABLES
 		double maara, dollari, euro, punta;
         DecimalFormat f = new DecimalFormat("##.##");
         Scanner sc = new Scanner(System.in);
 
+        // TITLE OF THE PROGRAM
         System.out.println("--- VALUUTANMUUNNIN ---");
         System.out.println("(Valuuttakurssit ovat laskettu 29.4.2020.)");
 		System.out.println("Anna luku: ");
 
+        // CORE OF THE PROGRAM
 		maara = sc.nextFloat();
             System.out.println(" ");
             System.out.println("----------------------------------");
@@ -40,12 +43,14 @@ public class muunnin {
             System.out.println("----------------------------------");
             System.out.println(" ");
 
+        // ARRAYLIST OF FUN FACTS
         ArrayList<String> randomiLause = new ArrayList<>();
         randomiLause.add("Fun fact: On olemassa yli 100 erilaista virusta, jotka aiheuttavat normaalin flunssan.");
         randomiLause.add("Fun fact: Bermudan kolmiossa ei tilastollisesti tapahdu normaalia enemmän onnettomuuksia.");
         randomiLause.add("Fun fact: 40% maailman viineistä valmistetaan Italiassa ja Ranskassa.");
         randomiLause.add("Fun fact: Bill Gates aloitti ohjelmoinnin 13-vuotiaana.");
         
+        // GENERATES RANDOMLY ONE OF FOUR SENTENCES
         int x = (int) (Math.random() * randomiLause.size());
         String y = randomiLause.get(x);
         System.out.println(y);
